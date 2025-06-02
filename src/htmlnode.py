abb648d86afb153422575a15e3efee02b0b4fdae
Edyth-K <kelvin.dcruz@gmail.com>
2025-06-02 13:09:48 -0500

    add htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,33 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        """
+        tag - A string representing the HTML tag name 
+            (e.g. "p", "a", "h1", etc.)
+        value - A string representing the value of the HTML tag 
+            (e.g. the text inside a paragraph)
+        children - A list of HTMLNode objects representing 
+            the children of this node
+        props - A dictionary of key-value pairs representing 
+            the attributes of the HTML tag. For example, a 
+            link (<a> tag) might have {"href": "https://www.google.com"}
+        """
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        output = ""
+        for attribute in self.props:
+            output += f"{attribute}={self.props[attribute]} "
+        return output[:-1]
+    
+    def __repr__(self):
+        print("HTMLNode:")
+        print(f"Tag: {self.tag}")
+        print(f"Value: {self.value}")
+        print(f"Children: {self.children}")
+        print(f"Props: {self.props}")
