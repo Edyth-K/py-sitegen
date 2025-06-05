be0a71a635ff523d07bced3b820cc87abed19337
Edyth-K <kelvin.dcruz@gmail.com>
2025-06-05 14:07:16 -0500

    added tests for extracting links/images from md

--- src/test_conversion.py
+++ src/test_conversion.py
@@ -1,6 +1,6 @@
 import unittest
 
-from conversion import text_node_to_html_node, split_nodes_delimiter
+from conversion import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
@@ -135,3 +135,21 @@ class TestConversion(unittest.TestCase):
             ],
             new_nodes,
         )
+
+def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+def test_extract_markdown_images2(self):
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    matches = extract_markdown_images(text)
+    self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+                         
+def test_extract_markdown_link(self):
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    matches = extract_markdown_links(text)
+    self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+
